2c741cb82b34ec281c4e04f8d82754882061e8fc
person896 <39915431+person896@users.noreply.github.com>
2018-06-13 17:31:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,25 @@
 var express = require('express');
 var compression = require('compression');
 var app = express();
-var http = require('http');
+var cors = require('cors');
+var https = require('https');
+var fs = require("fs");
 var ExpressPeerServer = require('peer').ExpressPeerServer;
-var wsServer = require('./server/ws-server.js');
+var wsServer = require('./ws-server.js');
 
-var server = http.createServer(app);
+var options = {
+  key:fs.readFileSync('/etc/ssl/server.key;'),
+  cert:fs.readFileSync('/etc/ssl/certificate.crt;')
+};
+
+var server = https.createServer(options,app);
 
 // Serve up content from public directory
+app.use(cors())
 app.use(compression());
 app.use(express.static(__dirname + '/public'));
 
-var port = process.env.PORT || 3002;
+var port = process.env.PORT || 3010;
 server.listen(port);
 wsServer.create(server);
 app.use('/peerjs', ExpressPeerServer(server, {
